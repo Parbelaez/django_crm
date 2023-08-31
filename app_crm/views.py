b182395f3fec77570b61239a46ab5d0f1c4d1309
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-08-31 21:16:53 +0200

    Fix the warning for the login

--- app_crm/views.py
+++ app_crm/views.py
@@ -1,10 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    # Check to see if logging in
+    if request.method == 'POST':
+        # Get the values from the form
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate the user
+        user = authenticate(request, username=username, password=password)
+        # If the user is valid
+        if user is not None:
+            # Log the user in
+            login(request, user)
+            # Send a message to the user
+            messages.success(request, 'You have been logged in!')
+            # Redirect the user to the home page
+            return redirect('home')
+        # If the user is not valid
+        else:
+            # Send a message to the user
+            messages.success(request, ('Error logging in - please try again...'))
+            # Redirect the user to the login page
+            return redirect('home')
+    # If not logging in
+    else:
+        # Render the home page
+        return render(request, 'home.html', {})
 
 def login_user(request):
     pass
